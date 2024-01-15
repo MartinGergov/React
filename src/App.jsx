7c124420ab4b8e5ff7d5edf5c73479ac69f0947d
Martin Gergov <martindgergov@gmail.com>
2024-01-15 14:01:47 +0100

    Ejercicio 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { Hello } from "./Hello";
 import { Message } from "./Message";
+import { Welcome } from "./Welcome";
 
 export function App() {
   return (
     <div>
       <Hello />
+      <hr />
       <Hello />
       {/* Puedes usarlo todas las veces que quieras */}
       <Message />
       {/* También se puede usar directamente en la App */}
       {/* Los dos se pueden usar siempre y cuando estén dentro de un solo contenedor padre */}
+      <hr />
+      <Welcome name="Martin" />
+      <hr />
     </div>
   );
 }
---
+++ src/Welcome.jsx
@@ -0,0 +1,4 @@
+export function Welcome({ name }) {
+  
+  return <p>Welcome {name}</p>;
+}
