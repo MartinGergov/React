9ee0bba14e0e16004a49bac2145aca6fbb235898
Martin Gergov <martindgergov@gmail.com>
2024-01-15 13:05:00 +0100

    Ejercicio 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { Hello } from "./Hello";
+import { Message } from "./Message";
 
 export function App() {
   return (
     <div>
       <Hello />
+      <Hello />
+      {/* Puedes usarlo todas las veces que quieras */}
+      <Message />
+      {/* También se puede usar directamente en la App */}
+      {/* Los dos se pueden usar siempre y cuando estén dentro de un solo contenedor padre */}
     </div>
   );
 }
