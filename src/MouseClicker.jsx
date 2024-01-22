3b97d499b8d6df367bfe441930a9d6bff676d560
Martin Gergov <martindgergov@gmail.com>
2024-01-22 17:09:31 +0100

    Ejercicio 16

--- src/MouseClicker.jsx
+++ src/MouseClicker.jsx
@@ -1,9 +1,10 @@
 export function MouseClicker() {
   function handleClick(event) {
-    console.log(event.target.name);
+    console.log(event.target.src);
   }
   return (
     <button name="one" onClick={handleClick}>
+      <img src="" width={30} height={30} onClick={handleClick} />
       Click here
     </button>
   );
