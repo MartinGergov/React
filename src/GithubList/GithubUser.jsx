2c41211526fe4fef208491a3bee3151d41e733e8
Martin Gergov <martindgergov@gmail.com>
2024-01-26 13:34:18 +0100

    Ejercicio 42

--- src/GithubList/GithubUser.jsx
+++ src/GithubList/GithubUser.jsx
@@ -2,12 +2,14 @@ import { useEffect } from "react";
 import { useGithubUser } from "./useGithubUser";
 
 export function GithubUser({ username }) {
-  const { data, fetchUser } = useGithubUser(username);
+  const { data, loading, error, fetchUser } = useGithubUser(username);
   useEffect(() => {
     fetchUser(username);
-  });
+  }, [username]);
   return (
     <div>
+      {error && <h3>There has been an error</h3>}
+      {loading && <h3>Loading...</h3>}
       {data && <h3>{data.name}</h3>}
       {data && <h3>{data.login}</h3>}
       {data && <h3>{data.avatar}</h3>}
--- src/GithubList/useGithubUser.jsx
+++ src/GithubList/useGithubUser.jsx
@@ -2,16 +2,22 @@ import { useState } from "react";
 
 export function useGithubUser() {
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function fetchUser(username) {
+    setLoading(true);
+    setError(null);
     try {
       const response = await fetch(`https://api.github.com/users/${username}`);
       const json = await response.json();
       setData(json);
     } catch (error) {
-      console.error(error);
+      setError(error);
+    } finally {
+      setLoading(false);
     }
   }
 
-  return { data, fetchUser };
+  return { data, loading, error, fetchUser };
 }
