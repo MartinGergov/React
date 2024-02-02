f652dfb4480bb79cf173a8242da2cc89a5604d4f
Martin Gergov <martindgergov@gmail.com>
2024-02-02 13:06:15 +0100

    Ejercicio Router 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Container } from "react-bootstrap";
 import { Route, Routes } from "react-router";
 import { Welcome } from "./Welcome";
+import { Counter } from "./Counter/Counter";
+import { Link } from "react-router-dom";
 
 export function App() {
   return (
     <Container>
+        <Link to="/">Home</Link> | <Link to="/counter">Counter</Link>
       <Routes>
         <Route path="/" element={<Welcome name="Martin" />} />
+        <Route path="/counter" element={<Counter />} />
       </Routes>
     </Container>
   );
