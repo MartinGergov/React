3f5160a11af8941d563f382bea9d197c44b33244
Martin Gergov <martindgergov@gmail.com>
2024-02-02 13:13:56 +0100

    Ejercicio Router 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import { Route, Routes } from "react-router";
 import { Welcome } from "./Welcome";
 import { Counter } from "./Counter/Counter";
 import { Link } from "react-router-dom";
+import { GithubUser } from "./GithubList/GithubUser";
 
 export function App() {
   return (
     <Container>
-        <Link to="/">Home</Link> | <Link to="/counter">Counter</Link>
+      <Link to="/">Home</Link> | <Link to="/counter">Counter</Link> |{" "}
+      <Link to="/users/:username">Github User</Link>
       <Routes>
         <Route path="/" element={<Welcome name="Martin" />} />
         <Route path="/counter" element={<Counter />} />
+        <Route path="/users/:username" element={<GithubUser username="MartinGergov" />} />
       </Routes>
     </Container>
   );
