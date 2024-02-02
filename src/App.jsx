e6510c45b34e13ca495c33906499e5d7e7231b73
Martin Gergov <martindgergov@gmail.com>
2024-02-02 12:52:46 +0100

    Ejercicio Router 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,13 @@
-import { useState } from "react";
-import { AlertClock } from "./AlertClock";
-import { Clock } from "./Clock/Clock";
-import { Colors } from "./Colors";
-import { Container } from "./Container";
-import { Counter } from "./Counter/Counter";
-import { FocusableInput } from "./FocusableInput";
-import { Hello } from "./Hello";
-import { InteractiveWelcome } from "./InteractiveWelcome";
-import { Login } from "./LoginForm/Login";
-import { Message } from "./Message";
-import { MouseClicker } from "./MouseClicker";
-import { TodoList } from "./TodoList/TodoList";
-import { UncontrolledLogin } from "./UncontrolledLogin";
+import { Container } from "react-bootstrap";
+import { Route, Routes } from "react-router";
 import { Welcome } from "./Welcome";
-import { LanguageContext } from "./LanguageContext";
-import { GithubUsers } from "./GithubList/GithubUsers";
-import { CurrentLocation } from "./CurrentLocation/CurrentLocation";
-import { FilteredList } from "./FilteredList";
 
 export function App() {
-  const [language, setLanguage] = useState("en");
-
-  function handleChangeLanguage(language) {
-    setLanguage(language);
-  }
-
-  const dataList = [
-    { id: 1, name: "Pedro", age: 34 },
-    { id: 2, name: "Carmen", age: 17 },
-    { id: 3, name: "Alicia", age: 22 },
-    { id: 4, name: "Jaime", age: 27 },
-  ];
   return (
-    <Container title={<h1>React</h1>}>
-      <Hello />
-      <hr />
-      <Hello />
-      {/* Puedes usarlo todas las veces que quieras */}
-      <Message />
-      {/* También se puede usar directamente en la App */}
-      {/* Los dos se pueden usar siempre y cuando estén dentro de un solo contenedor padre */}
-      <hr />
-      <Welcome name="Martin" />
-      <hr />
-      <AlertClock />
-      <hr />
-      <Counter />
-      <hr />
-      <LanguageContext.Provider value={language}>
-        <Clock />
-        <button onClick={() => handleChangeLanguage("es")}>ES</button>
-        <button onClick={() => handleChangeLanguage("en")}>EN</button>
-      </LanguageContext.Provider>
-      <hr />
-      <MouseClicker />
-      <hr />
-      <InteractiveWelcome />
-      <hr />
-      <Login />
-      <hr />
-      <UncontrolledLogin />
-      <hr />
-      <FocusableInput />
-      <hr />
-      <Colors
-        colorList={[
-          { id: 1, name: "Black" },
-          { id: 2, name: "Red" },
-          { id: 3, name: "Green" },
-          { id: 4, name: "Yellow" },
-        ]}
-      />
-      <hr />
-      <TodoList />
-      <hr />
-      <GithubUsers />
-      <hr />
-      <CurrentLocation />
-      <hr />
-      <FilteredList list={dataList} />
+    <Container>
+      <Routes>
+        <Route path="/" element={<Welcome name="Martin" />} />
+      </Routes>
     </Container>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,10 @@
-import { App } from "./App";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { StrictMode } from "react";
+import { Root } from "./Root";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Root />
+  </StrictMode>
+);
